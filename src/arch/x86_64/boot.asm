636d4cec4c5e4c77615e94bb7c4e61fc31f3579d
Jarrod Jeffrey Ingram <jarrod.jeffi@gmail.com>
2017-04-26 13:51:02 +1000

    Enter long mode

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,17 +1,18 @@
 global start
+extern long_mode_start
 
 section .text
 bits 32
 start:
         mov esp, stack_top
-
         call check_multiboot
         call check_cpuid
         call check_long_mode
-
-        ; Print 'OK' to the screen.
-        mov dword [0xb8000], 0x2f4b2f4f
-        hlt
+        call set_up_page_tables
+        call enable_paging
+        ; Load the 64-bit GDT
+        lgdt [gdt64.pointer]
+        jmp gdt64.code:long_mode_start
 
 ; Prints `ERR: ` and the given error code to screen and hangs.
 ;
@@ -84,7 +85,69 @@ check_long_mode:
         mov al, "2"
         jmp error
 
+
+; Present and writable.
+pw equ 0b11
+; Prensent, writable, and huge.
+pwh equ 0b10000011
+set_up_page_tables:
+        ; Map first P4 entry to P3 table.
+        mov eax, p3_table
+        or eax, pw
+        mov [p4_table], eax
+        ; Map first P3 entry to P2 table.
+        mov eax, p2_table
+        or eax, pw
+        mov [p3_table], eax
+        ; Map each P2 entry to a huge 2MiB page.
+        mov ecx, 0
+.map_p2_table:
+        ; Map ecx-th P2 entry to a huge page that starts at address 2MiB*ecx.
+        mov eax, 0x200000
+        mul ecx
+        or eax, pwh
+        mov [p2_table + ecx * 8], eax
+        inc ecx
+        cmp ecx, 512
+        jne .map_p2_table
+        ret
+
+enable_paging:
+        ; Load P4 to cr3 register (cpu uses this to access the P4 table).
+        mov eax, p4_table
+        mov cr3, eax
+        ; Enable PAE-flag in cr4 (Physical Address Extension).
+        mov eax, cr4
+        or eax, 1 << 5
+        mov cr4, eax
+        ; Set the long mode bit in the EFER MSR (model specific register).
+        mov ecx, 0xC0000080
+        rdmsr
+        or eax, 1 << 8
+        wrmsr
+        ; Enable paging in the cr0 register.
+        mov eax, cr0
+        or eax, 1 << 31
+        mov cr0, eax
+        ret
+
 section .bss
+align 4096
+p4_table:
+        resb 4096
+p3_table:
+        resb 4096
+p2_table:
+        resb 4096
 stack_bottom:
         resb 64
 stack_top:
+
+section .rodata
+gdt64:
+        dq 0
+.code: equ $ - gdt64
+        dq (1<<44) | (1<<47) | (1<<43) | (1<<53)
+.pointer:
+        dw $ - gdt64 - 1
+        dq gdt64
